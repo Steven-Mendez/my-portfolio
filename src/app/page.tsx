4ed6c2ac034fc88cd30c187d3aa3a84597e65209
Steven Mendez <stevenampaiz@gmail.com>
2025-01-05 01:30:12 -0600

    feat: Add Hero component and integrate it into the main page layout; update package dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import Footer from "@/components/footer";
+import Hero from "@/components/hero";
 import Navbar from "@/components/navbar";
 
 export default function Home() {
   return (
     <>
       <Navbar />
-      <main className="flex min-h-screen flex-col items-center justify-between"></main>
+      <main className="flex min-h-screen flex-col items-center justify-between">
+        <Hero />
+      </main>
       <Footer />
     </>
   );
